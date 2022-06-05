5370128f60b6d64fa4fd390383de96e09e8c045d
Selvapandian Subramani <103447755+sp-selva@users.noreply.github.com>
2022-06-05 23:52:54 +0530

    false file

--- update/src/components/PrivateRoutes.js
+++
@@ -1,9 +0,0 @@
-// import { useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouters = () => {
-    // const [isLogin, setIsLogin] = useState(false)
-  const auth = { token: true };
-  return auth.token ? <Outlet/> : <Navigate to="/" />;
-};
-export default PrivateRouters;
